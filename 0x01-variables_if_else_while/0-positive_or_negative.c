fa0f1094246c925420c7545bcc82870f299b05c0
Kyeyune Collin <kyeyunecollin90@gmail.com>
2023-02-20 05:31:32 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,15 +5,15 @@
 /**
  * main - prints if the numeber is positive, negative or 0
  * Return: always 0
- */
+*/
 
 int main(void)
 {
 	int n;
 
-	srand(time(o));
+	srand(time(0));
 
-	n = rand() RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
 	{
@@ -29,3 +29,4 @@ int main(void)
 	}
 
 	return (0);
+}
