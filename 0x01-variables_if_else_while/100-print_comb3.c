80844fe1c1aced4106188aee38f65e963683ccc2
Kyeyune Collin <kyeyunecollin90@gmail.com>
2023-02-26 03:50:46 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: program that prints all possible different combinations
+ * Two digits must be different
+ * The two digits must be different
+ * Numbers must be separated by ,, followed by a space
+ * Numbers should be printed in ascending order, with two digits
+ * Only use the putchar function
+ * Only use putchar five times maximum in code
+ * Return 0;
+ */
+int main(void)
+{
+	int c;
+	int d = 0;
+
+	while (d < 10)
+	{
+		c = 0;
+		while (c < 10)
+		{
+			if (d != c && d < c)
+			{
+				putchar('0' + c);
+				putchar('0' + d);
+
+				if (c + d != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			c++;
+		}
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
