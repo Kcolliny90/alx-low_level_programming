d7aedc2288ab250ec4d84f31b4fe06f1149be7be
Kyeyune Collin <kyeyunecollin90@gmail.com>
2023-03-02 22:45:37 -0800

    main.h

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * strncat - concatinate two strings
+ * using at most n bytes from src
+ * @dest: input value
+ * @src: input value
+ * @n: point of entry
+ *
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n) /*entry point for input value*/
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[1] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
