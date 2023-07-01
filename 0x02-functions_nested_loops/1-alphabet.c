09a64d3bf1ddc920929bcab14ab9162ad937c929
Kyeyune Collin <kyeyunecollin90@gmail.com>
2023-07-01 11:29:52 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - Prints the alphabet in lower case followe by new line
+ * print_alphabet - Prints the alphabet in lower case followe by new line
  * Return: void
  */
 
