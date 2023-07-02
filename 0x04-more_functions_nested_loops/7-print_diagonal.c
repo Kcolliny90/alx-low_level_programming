ad67cb9541473ecc644d9673dadb04bafc8d4b4b
Kyeyune Collin <kyeyunecollin90@gmail.com>
2023-07-01 17:28:04 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,12 +5,13 @@
  * @n - number of times the character \ should be printed
  */
 
-void print_diagonal (int n)
+void print_diagonal(int n)
 {
 	if (n <= 0)
 	{
 		_putchar('\n');
-	}else
+	}
+	else
 	{
 		int i, j;
 
